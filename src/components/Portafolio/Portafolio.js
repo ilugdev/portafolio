5a506fd94f703518aa5871a79f45117a3e9a99f5
Ilug <ilugdev@gmail.com>
2020-11-02 14:38:00 -0430

    best demo/code ux

--- src/components/Portafolio/Portafolio.js
+++ src/components/Portafolio/Portafolio.js
@@ -11,27 +11,24 @@ Portafolio.Box = function ({ data }) {
 
   return (
     <div className={style.portafolio}>
-      <input
-        className={style.mobileTouch}
-        type="checkbox"
-        name="mobile_touch"
-      />
       <img src={img} alt="project preview" />
       <div className={style.portafolioText}>
         <h1>{title}</h1>
-        <ul>
-          {tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </div>
-      <div className={style.menu}>
-        <a href={url} target="_blank" rel="noopener noreferrer">
-          DEMO
-        </a>
-        <a href={repo_url} target="_blank" rel="noopener noreferrer">
-          CODE
-        </a>
+        <div className={style.info}>
+          <ul className={style.techs}>
+            {tags.map((tag) => (
+              <li key={tag}>{tag}</li>
+            ))}
+          </ul>
+          <div className={style.projectsLinks}>
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              DEMO
+            </a>
+            <a href={repo_url} target="_blank" rel="noopener noreferrer">
+              CODE
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
